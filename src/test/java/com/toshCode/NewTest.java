99e6a8559c4663f7472154db5c1f4f9d44babb79
Santosh <santosh@Santoshs-MacBook-Pro.local>
2017-03-13 18:37:22 -0700

    Wrote the first test

--- src/test/java/com/toshCode/NewTest.java
+++ src/test/java/com/toshCode/NewTest.java
@@ -1,9 +1,19 @@
 package com.toshCode;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 import org.testng.annotations.Test;
 
 public class NewTest {
   @Test
   public void f() {
+	  System.setProperty("webdriver.chrome.driver", "/Users/santosh/Documents/workspace/FirstRepositorySantosh/chromedriver");
+	  
+	  
+	  WebDriver driver = new ChromeDriver();
+	  driver.get("http://www.credible.com");
+	  driver.manage().window().maximize();
+	  
   }
 }
